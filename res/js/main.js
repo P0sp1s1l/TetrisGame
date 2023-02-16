61f596b2d0363eab2dc89d3bc4b82d607c4429fe
P0sp1s1l <matej.pospisil@student.spsmb.cz>
2023-02-16 20:18:59 +0100

    fixed falling, better shapes, comments/explanation

--- res/js/main.js
+++ res/js/main.js
@@ -1,129 +1,192 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+//Canvas
+const canvas = document.getElementById("canvas"); //takes canvas from index.html body
+const ctx = canvas.getContext("2d"); //creates a context(ctx)
+canvas.width = 400; //width of canvas
+canvas.height = 800; //height of canvas
 
-blockSize = 10;
-let offset = 4;
+//Tetris problematic/logic
 
-//declaration of each shape from tetromino
+//start button
+
+//restart button
+const restart = document.getElementById("restart");
+restart.onclick = (event) => {
+  location.reload();
+};
+
+//size of 1 cell
+cellSize = 15;
+
+//the speed of falling tetromino
+let speedOftetromino = 1000;
+
+//Declaration of each shape from tetromino
 const tetrominoShape = {
+  //O
   O: [
     [1, 1],
     [1, 1],
   ],
 
+  //T
   T: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 0],
+    [0, 0, 0],
+    [2, 2, 2],
+    [0, 2, 0],
   ],
 
+  //I
   I: [
-    [0, 1, 0],
-    [0, 1, 0],
-    [0, 1, 0],
+    [0, 3, 0, 0],
+    [0, 3, 0, 0],
+    [0, 3, 0, 0],
+    [0, 3, 0, 0],
   ],
 
+  //J
   J: [
-    [1, 1, 1],
-    [0, 0, 1],
+    [4, 4, 4],
+    [0, 0, 4],
     [0, 0, 0],
   ],
 
+  //L
   L: [
-    [1, 1, 1],
-    [1, 0, 0],
+    [5, 5, 5],
+    [5, 0, 0],
     [0, 0, 0],
   ],
 
+  //S
   S: [
-    [0, 1, 1],
-    [1, 1, 0],
+    [0, 6, 6],
+    [6, 6, 0],
     [0, 0, 0],
   ],
 
+  //Z
   Z: [
-    [1, 1, 0],
-    [0, 1, 1],
+    [7, 7, 0],
+    [0, 7, 7],
     [0, 0, 0],
   ],
 };
 
-//color for each shape
-// - later upgrade to custom images.
-function colorizeTetromino(shape) {
-  switch (shape) {
-    default:
-      console.log(`unknown tetromino shape:${shape}`);
-      return;
 
-    case "O":
-      ctx.fillStyle = "yellow";
-      break;
-
-    case "T":
-      ctx.fillStyle = "purple";
-      break;
-
-    case "I":
-      ctx.fillStyle = "cyan";
-      break;
-
-    case "J":
-      ctx.fillStyle = "orange";
-      break;
-
-    case "L":
-      ctx.fillStyle = "blue";
-      break;
-
-    case "S":
-      ctx.fillStyle = "red";
-      break;
-
-    case "Z":
-      ctx.fillStyle = "green";
-      break;
-  }
-}
+//creates object with function fall and draw.
 class DrawTetromino {
-  constructor(x, y, shape) {
+  constructor(x, y, shape, canvas) {
     this.x = x;
     this.y = y;
     this.shape = shape;
+    this.canvas = canvas;
   }
 
+  //makes the tetromino fall
   fall() {
     this.y += 1;
     this.draw();
     setTimeout(() => {
       this.fall();
-    }, 1000);
+    }, speedOftetromino);
   }
 
+  //draws each type of tetromino
   draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
     this.shape.forEach((row, rowIndex) => {
       row.forEach((cell, columnIndex) => {
         if (cell === 1) {
+          ctx.fillStyle = "orange";
           const shapeX = this.x + columnIndex;
           const shapeY = this.y + rowIndex;
-
           ctx.fillRect(
-            shapeX * blockSize,
-            shapeY * blockSize,
-            blockSize,
-            blockSize
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 2) {
+          ctx.fillStyle = "purple";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 3) {
+          ctx.fillStyle = "cyan";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 4) {
+          ctx.fillStyle = "red";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 5) {
+          ctx.fillStyle = "blue";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 6) {
+          ctx.fillStyle = "yellow";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
+          );
+        }
+        if (cell === 7) {
+          ctx.fillStyle = "green";
+          const shapeX = this.x + columnIndex;
+          const shapeY = this.y + rowIndex;
+          ctx.fillRect(
+            shapeX * cellSize,
+            shapeY * cellSize,
+            cellSize,
+            cellSize
           );
         }
       });
     });
   }
 }
-const testing = new DrawTetromino(5, 5, []);
+//creates tetromino object
+const run = new DrawTetromino(8, 0, []);
+//gets ID´s from tetrominoShape
 const typesOfTetromino = Object.keys(tetrominoShape);
+//randomize tetromino types
 const randomizer =
   typesOfTetromino[Math.floor(Math.random() * typesOfTetromino.length)];
 tetrominoShape[randomizer].forEach((row) => {
-  testing.shape.push([...row]);
+  run.shape.push([...row]);
 });
-testing.fall();
-update();
+
+//print function fall, from constructor, to website
+run.fall();
